59741ae740ea4e7a2018e280a6f206b00f103e3b
Ahmad Wilson <ahwilson712@gmail.com>
2020-03-20 15:46:08 -0400

    update default listening port and apply gofmt'ing

--- src/gather.go
+++ src/gather.go
@@ -1,110 +1,111 @@
 package main
 
 import (
-    "strings"
-    "google.golang.org/api/compute/v1"
 	log "github.com/Sirupsen/logrus"
 	"github.com/prometheus/client_golang/prometheus"
+	"google.golang.org/api/compute/v1"
+	"strings"
 )
 
 func getZonesFromRegion(region string) []string {
 
-    result, err := computeService.Regions.Get(project, region).Context(ctx).Do()
-    if err != nil {
-        log.Fatal(err)
-    }
+	result, err := computeService.Regions.Get(project, region).Context(ctx).Do()
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    var zones []string;
-    for _, zone := range result.Zones {
-        s := strings.Split(zone, "/")
-        z := s[len(s)-1]
-        zones = append(zones, z)
-    }
+	var zones []string
+	for _, zone := range result.Zones {
+		s := strings.Split(zone, "/")
+		z := s[len(s)-1]
+		zones = append(zones, z)
+	}
 
-    return zones
+	return zones
 }
-    
 
 func (e *Exporter) gatherInstanceMetrics(ch chan<- prometheus.Metric) (*compute.MachineTypesListCall, error) {
 
-    // Process all zones contained within the specified REGION
-    zones := getZonesFromRegion(region)
+	// Process all zones contained within the specified REGION
+	zones := getZonesFromRegion(region)
 
-    var err error; var result *compute.MachineTypesListCall;
-    for _, zone := range zones {
-        result = computeService.MachineTypes.List(project, zone)
-        if err = result.Pages(ctx, func(page *compute.MachineTypeList) error {
-            log.Debug("MachineTypes <RESULT>:", page.Items)
-            for _, machineType := range page.Items {
-                log.Debug("Data <machine>:", machineType)
-                log.Debug("Zone <zone>:", zone)
+	var err error
+	var result *compute.MachineTypesListCall
+	for _, zone := range zones {
+		result = computeService.MachineTypes.List(project, zone)
+		if err = result.Pages(ctx, func(page *compute.MachineTypeList) error {
+			log.Debug("MachineTypes <RESULT>:", page.Items)
+			for _, machineType := range page.Items {
+				log.Debug("Data <machine>:", machineType)
+				log.Debug("Zone <zone>:", zone)
 
-                s := strings.Split(machineType.Zone, "/")
-                z := s[len(s)-1]
+				s := strings.Split(machineType.Zone, "/")
+				z := s[len(s)-1]
 
-                e.gaugeVecs["totalvCPUs"].With(prometheus.Labels{
-                    "instance_type": machineType.Name,
-                    "region":        region,
-                    "zone":          z,
-                }).Set(float64(machineType.GuestCpus))
-                e.gaugeVecs["totalMem"].With(prometheus.Labels{
-                    "instance_type": machineType.Name,
-                    "region":        region,
-                    "zone":          z,
-                }).Set(float64(machineType.MemoryMb))
-                e.gaugeVecs["maxStorage"].With(prometheus.Labels{
-                    "instance_type": machineType.Name,
-                    "region":        region,
-                    "zone":          z,
-                }).Set(float64(machineType.MaximumPersistentDisksSizeGb))
-                e.gaugeVecs["maxDisks"].With(prometheus.Labels{
-                    "instance_type": machineType.Name,
-                    "region":        region,
-                    "zone":          z,
-                }).Set(float64(machineType.MaximumPersistentDisks))
-            }
+				e.gaugeVecs["totalvCPUs"].With(prometheus.Labels{
+					"instance_type": machineType.Name,
+					"region":        region,
+					"zone":          z,
+				}).Set(float64(machineType.GuestCpus))
+				e.gaugeVecs["totalMem"].With(prometheus.Labels{
+					"instance_type": machineType.Name,
+					"region":        region,
+					"zone":          z,
+				}).Set(float64(machineType.MemoryMb))
+				e.gaugeVecs["maxStorage"].With(prometheus.Labels{
+					"instance_type": machineType.Name,
+					"region":        region,
+					"zone":          z,
+				}).Set(float64(machineType.MaximumPersistentDisksSizeGb))
+				e.gaugeVecs["maxDisks"].With(prometheus.Labels{
+					"instance_type": machineType.Name,
+					"region":        region,
+					"zone":          z,
+				}).Set(float64(machineType.MaximumPersistentDisks))
+			}
 
-            return nil
-            
-        }); err != nil {
-             log.Fatal(err)
-        }
-    }
+			return nil
 
-    return result, err
+		}); err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	return result, err
 }
 
 func (e *Exporter) gatherImageMetrics(ch chan<- prometheus.Metric) (*compute.ImagesListCall, error) {
 
-    var err error; var result *compute.ImagesListCall;
-    for _, proj := range imageProjects {
-        result := computeService.Images.List(proj)
-        if err := result.Pages(ctx, func(page *compute.ImageList) error {
-            log.Debug("ImageList <RESULT>:", page.Items)
-            for _, image := range page.Items {
-                log.Debug("Data <image>:", image)
+	var err error
+	var result *compute.ImagesListCall
+	for _, proj := range imageProjects {
+		result := computeService.Images.List(proj)
+		if err := result.Pages(ctx, func(page *compute.ImageList) error {
+			log.Debug("ImageList <RESULT>:", page.Items)
+			for _, image := range page.Items {
+				log.Debug("Data <image>:", image)
 
-                e.gaugeVecs["imgArchiveSize"].With(prometheus.Labels{
-                    "name":          image.Name,
-                    "family":        image.Family,
-                    "src_img":       image.SourceImage,
-                }).Set(float64(image.ArchiveSizeBytes))
-                e.gaugeVecs["imgDiskSize"].With(prometheus.Labels{
-                    "name":          image.Name,
-                    "family":        image.Family,
-                    "src_img":       image.SourceImage,
-                }).Set(float64(image.DiskSizeGb))
-                e.counterVecs["totalImages"].With(prometheus.Labels{
-                    "family":        image.Family,
-                    "src_img":       image.SourceImage,
-                }).Inc()
-            }
+				e.gaugeVecs["imgArchiveSize"].With(prometheus.Labels{
+					"name":    image.Name,
+					"family":  image.Family,
+					"src_img": image.SourceImage,
+				}).Set(float64(image.ArchiveSizeBytes))
+				e.gaugeVecs["imgDiskSize"].With(prometheus.Labels{
+					"name":    image.Name,
+					"family":  image.Family,
+					"src_img": image.SourceImage,
+				}).Set(float64(image.DiskSizeGb))
+				e.counterVecs["totalImages"].With(prometheus.Labels{
+					"family":  image.Family,
+					"src_img": image.SourceImage,
+				}).Inc()
+			}
 
-            return nil
-        }); err != nil {
-             log.Fatal(err)
-        }
-    }
+			return nil
+		}); err != nil {
+			log.Fatal(err)
+		}
+	}
 
 	return result, err
 
@@ -112,36 +113,37 @@ func (e *Exporter) gatherImageMetrics(ch chan<- prometheus.Metric) (*compute.Ima
 
 func (e *Exporter) gatherDiskMetrics(ch chan<- prometheus.Metric) (*compute.DiskTypesListCall, error) {
 
-    // Process all zones contained within the specified REGION
-    zones := getZonesFromRegion(region)
+	// Process all zones contained within the specified REGION
+	zones := getZonesFromRegion(region)
 
-    var err error; var result *compute.DiskTypesListCall;
-    for _, zone := range zones {
-        result := computeService.DiskTypes.List(project, zone)
-        if err := result.Pages(ctx, func(page *compute.DiskTypeList) error {
-            log.Debug("DiskTypeList <RESULT>:", page.Items)
-            for _, dt := range page.Items {
-                log.Debug("Data <image>:", dt)
+	var err error
+	var result *compute.DiskTypesListCall
+	for _, zone := range zones {
+		result := computeService.DiskTypes.List(project, zone)
+		if err := result.Pages(ctx, func(page *compute.DiskTypeList) error {
+			log.Debug("DiskTypeList <RESULT>:", page.Items)
+			for _, dt := range page.Items {
+				log.Debug("Data <image>:", dt)
 
-                e.gaugeVecs["diskTypeSize"].With(prometheus.Labels{
-                    "name":          dt.Name,
-                    "valid_disk_size": dt.ValidDiskSize,
-                    "region":        region,
-                    "zone":          zone,
-                }).Set(float64(dt.DefaultDiskSizeGb))
-                e.counterVecs["totalDiskTypes"].With(prometheus.Labels{
-                    "valid_disk_size": dt.ValidDiskSize,
-                    "region":        region,
-                    "zone":          zone,
-                }).Inc()
+				e.gaugeVecs["diskTypeSize"].With(prometheus.Labels{
+					"name":            dt.Name,
+					"valid_disk_size": dt.ValidDiskSize,
+					"region":          region,
+					"zone":            zone,
+				}).Set(float64(dt.DefaultDiskSizeGb))
+				e.counterVecs["totalDiskTypes"].With(prometheus.Labels{
+					"valid_disk_size": dt.ValidDiskSize,
+					"region":          region,
+					"zone":            zone,
+				}).Inc()
 
-            }
+			}
 
-            return nil
-        }); err != nil {
-             log.Fatal(err)
-        }
-    }
+			return nil
+		}); err != nil {
+			log.Fatal(err)
+		}
+	}
 
 	return result, err
 
@@ -149,23 +151,23 @@ func (e *Exporter) gatherDiskMetrics(ch chan<- prometheus.Metric) (*compute.Disk
 
 func (e *Exporter) gatherRegionMetrics(ch chan<- prometheus.Metric) (*compute.RegionsListCall, error) {
 
-    var err error;
-    result := computeService.Regions.List(project)
-    if err := result.Pages(ctx, func(page *compute.RegionList) error {
-        log.Debug("RegionList <RESULT>:", page.Items)
-        for _, region := range page.Items {
-            log.Debug("Data <region>:", region)
+	var err error
+	result := computeService.Regions.List(project)
+	if err := result.Pages(ctx, func(page *compute.RegionList) error {
+		log.Debug("RegionList <RESULT>:", page.Items)
+		for _, region := range page.Items {
+			log.Debug("Data <region>:", region)
 
-            e.counterVecs["totalRegions"].With(prometheus.Labels{
-                "name": region.Name,
-                "status": region.Status,
+			e.counterVecs["totalRegions"].With(prometheus.Labels{
+				"name":   region.Name,
+				"status": region.Status,
 			}).Inc()
-        }
+		}
 
-        return nil
-    }); err != nil {
-         log.Fatal(err)
-    }
+		return nil
+	}); err != nil {
+		log.Fatal(err)
+	}
 
 	return result, err
 
@@ -173,27 +175,27 @@ func (e *Exporter) gatherRegionMetrics(ch chan<- prometheus.Metric) (*compute.Re
 
 func (e *Exporter) gatherZoneMetrics(ch chan<- prometheus.Metric) (*compute.ZonesListCall, error) {
 
-    var err error;
-    result := computeService.Zones.List(project)
-    if err := result.Pages(ctx, func(page *compute.ZoneList) error {
-        log.Debug("ZoneList <RESULT>:", page.Items)
-        for _, zone := range page.Items {
-            log.Debug("Data <zone>:", zone)
+	var err error
+	result := computeService.Zones.List(project)
+	if err := result.Pages(ctx, func(page *compute.ZoneList) error {
+		log.Debug("ZoneList <RESULT>:", page.Items)
+		for _, zone := range page.Items {
+			log.Debug("Data <zone>:", zone)
 
-            s := strings.Split(zone.Region, "/")
-            r := s[len(s)-1]
+			s := strings.Split(zone.Region, "/")
+			r := s[len(s)-1]
 
-            e.counterVecs["totalZones"].With(prometheus.Labels{
-                "name": zone.Name,
-                "status": zone.Status,
-                "region": r,
+			e.counterVecs["totalZones"].With(prometheus.Labels{
+				"name":   zone.Name,
+				"status": zone.Status,
+				"region": r,
 			}).Inc()
-        }
-        
-        return nil
-    }); err != nil {
-         log.Fatal(err)
-    }
+		}
+
+		return nil
+	}); err != nil {
+		log.Fatal(err)
+	}
 
 	return result, err
 
--- src/main.go
+++ src/main.go
@@ -5,11 +5,11 @@ import (
 
 	c "github.com/0x0I/gcp-gce-exporter/src/config"
 	log "github.com/Sirupsen/logrus"
-    "golang.org/x/net/context"
-    "golang.org/x/oauth2/google"
-    "google.golang.org/api/compute/v1"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"golang.org/x/net/context"
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/compute/v1"
 )
 
 const (
@@ -19,46 +19,46 @@ const (
 // Runtime variables
 var (
 	metricsPath = c.GetEnv("METRICS_PATH", "/metrics") // Path under which to expose metrics
-	listenPort  = c.GetEnv("LISTEN_PORT", ":9687")     // Port on which to expose metrics
+	listenPort  = c.GetEnv("LISTEN_PORT", ":9692")     // Port on which to expose metrics
 	logLevel    = c.GetEnv("LOG_LEVEL", "info")
 
-	region      = c.GetEnv("REGION", "us-east1")
-    project     = c.GetEnv("PROJECT", "")
+	region  = c.GetEnv("REGION", "us-east1")
+	project = c.GetEnv("PROJECT", "")
 
-    // GCP public image projects with and without Shielded VM support
-    imageProjects []string = []string{
-        "gce-uefi-images",
-        "centos-cloud",
-        "cos-cloud",
-        "coreos-cloud",
-        "debian-cloud",
-        "rhel-cloud",
-        "rhel-sap-cloud",
-        "suse-cloud",
-        "suse-sap-cloud",
-        "ubuntu-os-cloud",
-        "windows-cloud",
-        "windows-sql-cloud",
-    }
+	// GCP public image projects with and without Shielded VM support
+	imageProjects []string = []string{
+		"gce-uefi-images",
+		"centos-cloud",
+		"cos-cloud",
+		"coreos-cloud",
+		"debian-cloud",
+		"rhel-cloud",
+		"rhel-sap-cloud",
+		"suse-cloud",
+		"suse-sap-cloud",
+		"ubuntu-os-cloud",
+		"windows-cloud",
+		"windows-sql-cloud",
+	}
 
-    // Google API access global variables for single context
-    ctx = context.Background()
-    computeClient *http.Client
-    computeService *compute.Service
+	// Google API access global variables for single context
+	ctx            = context.Background()
+	computeClient  *http.Client
+	computeService *compute.Service
 )
 
 func main() {
 	c.CheckConfig()
 
-    var err error
-    computeClient, err = google.DefaultClient(ctx, compute.CloudPlatformScope)
-    if err != nil {
-        log.Fatal(err)
-    }
-    computeService, err = compute.New(computeClient)
-    if err != nil {
-        log.Fatal(err)
-    }
+	var err error
+	computeClient, err = google.DefaultClient(ctx, compute.CloudPlatformScope)
+	if err != nil {
+		log.Fatal(err)
+	}
+	computeService, err = compute.New(computeClient)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	setLogLevel(logLevel)
 	log.Info("Starting Prometheus GCP GCE Exporter")
